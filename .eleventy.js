82d82d91cb013d18c8d0a8f4a8457cdf46f354a6
leyla_qiao <775177772@qq.com>
2025-03-08 19:43:26 +0800

    learn

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,82 @@
-module.exports = function (eleventyConfig) {
-  // 复制静态资源
+module.exports = function(eleventyConfig) {
+  // 复制静态资源     
   eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addPassthroughCopy("src/js");
+
+  // 添加 Nunjucks 过滤器
+  eleventyConfig.addFilter("year", () => {
+    return new Date().getFullYear();
+  });
+
+  // common filter
+  eleventyConfig.addFilter("myFilter", (date) => {
+    return "hello world";
+  });
+
+  // Nunjucks Filter
+	eleventyConfig.addFilter(
+		"concatThreeThings",
+		function (arg1, arg2, arg3) {
+			return arg1 + arg2 + arg3;
+		}
+	);
+
+  // async filter
+  eleventyConfig.addAsyncFilter("asyncFilter", async (date) => {
+    return "hello world";
+  });
+  eleventyConfig.addAsyncFilter("asyncFilter2", async (date) => {
+    return "hello world";
+  });
+
+  // Universal Shortcodes (Adds to Liquid, Nunjucks, 11ty.js)
+  eleventyConfig.addShortcode("user", function(name, twitterUsername) {
+    return `<div class="user">
+<div class="user_name">${name}</div>
+<div class="user_twitter">@${twitterUsername}</div>
+</div>`;
+  });
+
+   // Universal Shortcodes (Adds to Liquid, Nunjucks, 11ty.js)
+   eleventyConfig.addPairedShortcode("userUniversal", function(bioContent, name, twitterUsername) {
+    return `<div class="user">
+<div class="user_name">${name}</div>
+<div class="user_twitter">@${twitterUsername}</div>
+<div class="user_bio">${bioContent}</div>
+</div>`;
+  });
+
+  // Nunjucks Shortcode
+	eleventyConfig.addShortcode("user3", function (user) {
+		return `<div class="user">
+<div class="user_name">${user.name}</div>
+${user.twitter ? `<div class="user_twitter">@${user.twitter}</div>` : ""}
+</div>`;
+	});
+
+  // set 
+  eleventyConfig.addShortcode("myShortcode", function () {
+    return `hello world===========================`;
+  });
+
+  // setAsync
+  eleventyConfig.addAsyncShortcode("myAsyncShortcode", async () => {
+    return "hello world-----------------------";
+  });
+
+
+  eleventyConfig.addGlobalData("myGlobalData", 33);
+
+  eleventyConfig.addNunjucksGlobal("fortythree", 43);
+  // setAsync
+  eleventyConfig.addNunjucksGlobal("fortytwo", function () {
+		return 42;
+	});
 
   return {
-    dir: {
-      input: "src",
-      output: "_site"
-    }
+      dir: {
+          input: "src",
+          output: "_site"
+      }
   };
 };
--- README.md
+++ README.md
@@ -9,3 +9,45 @@ npx @11ty/eleventy --serve
 ```
 npx @11ty/eleventy
 ```
+
+## use 
+
+### use filter
+1. 可以在 `.eleventy.js`文件中配置`过滤器`，
+  - 同步`过滤器`api: `addFilter`
+  - 异步`过滤器`api: `addAsyncFilter`
+
+2. 在njk中引用时，用`{{}}`包裹
+
+```njk
+ <p>{{ "now2" | myFilter }}</p>
+```
+
+### use shortcode
+
+1. 可以在 `.eleventy.js`文件中配置`代码片段`，
+  - 同步`代码片段` api `addShortcode`
+  - 异步`代码片段` api `addAsyncShortcode`
+  - 成对`代码片段` api `addPairedShortcode`，`addPairedAsyncShortcode`
+
+2. 在njk中引用时，用`{% %}`包裹
+```njk
+{% user "Zach Leatherman", "zachleat" %} 
+
+{% userUniversal "Zach Leatherman", "zachleat" %} 
+  Zach likes to take long walks on Nebraska beaches. 
+{% enduserUniversal %} 
+
+{% user3 name="Zach Leatherman", twitter="zachleat" %}
+
+```
+
+## 注意事项
+- 本地开发时，在 `.eleventy.js`文件增加`过滤器`、`代码片段`后，需要重新运行`npx @11ty/eleventy --serve`命令，否则在使用时会报错
+```
+eleventyConfig.addAsyncFilter("asyncFilter2", async (date) => {
+    return "hello world";
+  });
+```
+
+
